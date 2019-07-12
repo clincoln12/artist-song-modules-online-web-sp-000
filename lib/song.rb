7d21f0d8e30bdcc0a233c45981ec289b922739af
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-12 18:38:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
   
-  extend Memorable :: ClassMethods
-  extend Findable :: ClassMethods
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
 
   @@songs = []
 
