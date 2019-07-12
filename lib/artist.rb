6d6b0fd9a133bafc3838d5139c6fc84dc457943e
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-12 18:34:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
-  extend Findable
+  
+  extend Memorable :: ClassMethods
+  extend Findable :: ClassMethods
 
   @@artists = []
 
